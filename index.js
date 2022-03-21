5ca544d0702988eb9682ab422f62c071b1a14f57
Rachel <rstark.smith@gmail.com>
2022-03-21 16:13:09 -0500

    commit tree bb9df2cb64f44fa9a44da6e2128c987837135ee9

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+// function destructivelyAppendCat(){
+//    return cats.push('Ralph');
+// }
+const destructivelyAppendCat = () => cats.push('Ralph');
+
+// function destructivelyRemoveLastCat(){
+//     return cats.pop();
+// }
+
+const destructivelyRemoveLastCat = () => cats.pop();
+
+// function destructivelyPrependCat(){
+//     return cats.unshift('Bob');
+// }
+
+const destructivelyPrependCat = () => cats.unshift('Bob');
+
+// function destructivelyRemoveFirstCat(){
+//     return cats.shift();
+// }
+const destructivelyRemoveFirstCat = () => cats.shift();
+
+function appendCat() {
+    const newCats = [...cats, 'Broom'];
+    return newCats;
+}
+
+function prependCat(){
+    const newCatsAdd = ['Arnold', ...cats];
+    return newCatsAdd;
+}
+
+function removeLastCat(){
+    const newCatsEdit = cats.slice(0, 2);
+    return newCatsEdit;
+}
+
+function removeFirstCat(){
+    const newCatsChange = cats.slice(1, 3);
+    return newCatsChange;
+}
